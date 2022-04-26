cf7fd518a6bfb54916d6b96877025fc18b6a4036
Seb <seb@Sebs-MacBook-Air.local>
2022-04-26 13:29:44 -0700

    comments and initialization

---
+++ fileRecieveSystem.py
@@ -0,0 +1,9 @@
+#===========================================================================#
+# Authors:  Sebastian Reel, Keegan Graf, Kim Meza Martinez                  #
+# Course:   CPE 400: Computer Communication Networks                        #
+# Project:  Final Project - Implement a file transfer system                #
+# DueL      May 2nd, 2022                                                   #
+#===========================================================================#
+
+import socket
+
---
+++ fileSendSystem.py
@@ -0,0 +1,28 @@
+#===========================================================================#
+# Authors:  Sebastian Reel, Keegan Graf, Kim Meza Martinez                  #
+# Course:   CPE 400: Computer Communication Networks                        #
+# Project:  Final Project - Implement a file transfer system                #
+# DueL      May 2nd, 2022                                                   #
+#===========================================================================#
+
+import socket
+import os
+import sys
+
+def func1():
+    ()
+
+def func2():
+    ()
+
+def func3():
+    ()
+
+def main():
+    print("Hello.")
+    func1()
+    func2()
+    func3()
+
+if __name__ in "__main__":
+    main()
