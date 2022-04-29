3e743f05c350f2b1e487d8e99e5cdb5b0dd8c4c1
Keegers101 <graf.keegan@gmail.com>
2022-04-28 23:54:26 -0700

    Add files via upload

--- fileRecieveSystem.py
+++ fileRecieveSystem.py
@@ -7,3 +7,36 @@
 
 import socket
 
+import os
+
+
+host = "0.0.0.0"
+port = 4891
+buffer = 4096
+sep = "<SEPARATOR>"
+
+s = socket.socket()
+
+s.bind((host, port))
+
+s.listen(5)
+print(f"[*] Listening as {host}:{port}")
+
+c_socket, address = s.accept()
+print(f"[+] {address} is connected.")
+
+received = c_socket.recv(buffer).decode()
+filename, filesize = received.split(sep)
+filename = os.path.basename(filename)
+
+filesize = int(filesize)
+
+with open(filename, "wb") as f:
+    while True:
+        b_read = c_socket.recv(buffer)
+        if not b_read:
+            break
+        f.write(b_read)
+
+c_socket.close()
+s.close()
