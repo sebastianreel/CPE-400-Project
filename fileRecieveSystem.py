d34980d6a8cb723b0957e3302d4d722649cd1a0e
reallyseb <reallyseb@github.com>
2022-05-04 17:00:39 -0700

    testing, code doc, few changes

--- fileRecieveSystem.py
+++ fileRecieveSystem.py
@@ -5,47 +5,77 @@
 # DueL      May 2nd, 2022                                                   #
 #===========================================================================#
 
+# import specific libraries from Python to use for the receiving functionality
 import socket
 import threading
 import os
 
-
+# VARIABLES
+# First grab the host computer name to use when identifying what is being listened and connected to
 host = socket.gethostname()
+# Define a base port to connect to
 port = 4891
+# The file size amount that the reciever will accept (in bytes)
 buffer = 4096
-sep = "||"
+count = 0
 
+# Define a socket, where the system will bind the host to the port and listen constantly
+# Then, print out that the server is being listened to, so that that the Send System can successfully work.
 s = socket.socket()
-
 s.bind((host, port))
-
 s.listen(10)
 print(f"[*] Listening as {host}:{port}")
 
-
-while True:
+# RECEIVE FUNCTIONALITY
+# run through a loop, this way multiple files can be passed through with one run
+while count != 5:
+    # after the "fileSendSystem" program has been run, then this loop will be activated
+    # the address will be defined and connected to the socket defined before
     c_socket, address = s.accept()
     print(f"[+] {address} is connected.")
 
-    received = c_socket.recv(buffer).decode()
-    filename, filesize = received.split(sep)
+    # First, look at what the socket recieved in terms of the directory path and size of the file being moved.
+    received = c_socket.recv(buffer).decode()    
+    # split the new string into two seperate string lists, with the filename and filesize
+    filename, filesize = received.split("||")
+    # take the name and only take the final component of the path name, which is a function the "os" library uses.
+    # this library is useful for being able to easily connect pathways and parse through directories faster
     filename = os.path.basename(filename)
-    filesize = int(filesize)
-    currentDirectory = os.getcwd()			#create folder in current directory
-    directoryName = 'myfolder'
+    # make the filesize, a string within a list of strings, an int instead
+    # filesize = int(filesize)
+    print("[+] File name: " + filename)
+    print("[+] File size:" + filesize)
+    
+    # get the current directory that the user is on within their current device
+    currentDirectory = os.getcwd()
+    # name a new directroy where the files are going to be transferred to
+    directoryName = 'test_recieve'
+    # create a list of strings that shows all of the items in the current directory
     stuff = os.listdir(currentDirectory)
+    
+    # condition to check that the receive directory is in the current directory
+    # join said directroy togetehr so that we can add files into it
     if directoryName not in stuff:
         path = os.path.join(currentDirectory, directoryName)
         os.mkdir(path)
     else:
         path = os.path.join(currentDirectory, directoryName)
+    
+    #
+    # add code documentation here
     with open(filename, "wb") as f:
+        joiner = os.path.join(path, filename)
+
         while True:
             b_read = c_socket.recv(buffer)
+            print(b_read)
             if not b_read:
                 break
             f.write(b_read)
     c_socket.close()
-
-
-        
+    
+    # iterate upwards and count to the number of files in the directory (5 in this case)
+    # set a condition to where the program ends if the number of files has been successfully reached
+    count+=1
+    if count == 5:
+        exit()
--- fileSendSystem.py
+++ fileSendSystem.py
@@ -5,46 +5,68 @@
 # DueL      May 2nd, 2022                                                   #
 #===========================================================================#
 
+# import specific libraries from Python to use for the receiving functionality
 import socket
 import os
 import argparse
 
-SEPERATOR = "||"
 BUFFER_SIZE = 1024 * 4 
 
+# SENDFILE FUNCTIONALITY
 def sendFile(dirname, host, port):
+	# get the current directory and the items within it
+	cwd = os.getcwd()
+	incwd = os.listdir(cwd)
 	
-
-	#sends dirname & size
-	stuff = os.getcwd()
-	inStuff = os.listdir(stuff)
-	if dirname in inStuff:
+	# create a condition: find that the dirname is within the current directory
+	if dirname in incwd:
+    	# get the items within the directory we are sending data from
 		items = os.listdir(dirname)
+		
+		# loop through the range of the files
 		for i in range(len(items)):
-			pathy = os.path.join(stuff, dirname)
+    		# connect the path to throughout into the items trhough each iteration of the loop
+			pathy = os.path.join(cwd, dirname)
 			pathx = os.path.join(pathy, items[i])
+			# define the socket
 			s = socket.socket()
 			
-			print(f"[+] Connecting to {host}:{port}")
+			# show what host name and port that the user is connecting to then connect them
+			print(f"[*] Connecting to {host}:{port}")
 			s.connect((host, port))
-			print("[+] Connected.")			
+			print("[+] Connected.")
+
+			# get the filesize of each of the files within the "send" directory
 			filesize = os.path.getsize(pathx)
-			s.send(f"{pathx}{SEPERATOR}{filesize}".encode())
+			s.send(f"Pathway: {pathx} || {filesize} bytes".encode())
+		
+			#
+			# add code documentation here
 			with open (pathx, "rb") as f:
 				while True:
 					bytesRead = f.read(BUFFER_SIZE)
 					if not bytesRead:
 						break	#file transmit done
-						
 					s.sendall(bytesRead)		#assures transmission in busy networks
 	s.close()
+
+
 def main():
-	parser = argparse.ArgumentParser(description="Simple File Sender")
+	# parse through the user imput, and if something is not correct, then print said output
+	# add an arguement for giving the user information on how to run the program correctly
+	parser = argparse.ArgumentParser(description = "Simple File Sender")
 	parser.add_argument("directory", help="Directory name to send")
+	
+	# parse through what the user enters the correct item (in this case a directory name)
 	args = parser.parse_args()
 	dirname = args.directory
+
+	# gett he host and define the same port id for using in sending files
 	host = socket.gethostname()
 	port = 4891
+
+	# call the function for sending files nad pass through the directory entered, the host name, and the port id to use in the functionality
 	sendFile(dirname, host, port)
+
 if __name__ in "__main__":
 	main()
